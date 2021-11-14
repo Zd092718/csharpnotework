15aa3cff25c08703b937d5621616a9a8b8b8b7ec
Zachary Dowd <zacharyd2796@zohomail.com>
2021-11-14 16:47:36 -0700

    zoo sql work continued

--- WpfAppZooSQL/WpfAppZooSQL/MainWindow.xaml.cs
+++ WpfAppZooSQL/WpfAppZooSQL/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace WpfAppZooSQL
 {
@@ -21,12 +23,36 @@ namespace WpfAppZooSQL
     /// </summary>
     public partial class MainWindow : Window
     {
+        SqlConnection sqlConnection;
         public MainWindow()
         {
             InitializeComponent();
 
             string connectionString = ConfigurationManager.ConnectionStrings
                 ["WpfAppZooSQL.Properties.Settings.C_DBCLASSConnectionString"].ConnectionString;
+            
+            sqlConnection = new SqlConnection(connectionString);
+
+
+        }
+
+        private void ShowZoos()
+        {
+            string query = "select * from Zoo";
+            //sqldataAdapter can be seen as an interface to make tables usable by c sharp objects
+            SqlDataAdapter adapter = new SqlDataAdapter(query, sqlConnection);
+
+            using (adapter)
+            {
+                DataTable zooTable = new DataTable();
+
+                adapter.Fill(zooTable);
+
+                ListofZoos.DisplayMemberPath = "location";
+                ListofZoos.SelectedValuePath = "Id";
+                ListofZoos.ItemsSource = zooTable.DefaultView;
+
+            }
         }
     }
 }
