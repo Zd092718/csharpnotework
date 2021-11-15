93e1395e1f2649ae06df8c56722f75b2c408070f
Zachary Dowd <zacharyd2796@zohomail.com>
2021-11-15 13:41:00 -0700

    XML to Linq notes

---
+++ LinqWithXML/LinqWithXML/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace LinqWithXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //We simply apply our Student-Structure to XML. 
+            string studentsXML =
+                        @"<Students>
+                            <Student>
+                                <Name>Toni</Name>
+                                <Age>21</Age>
+                                <University>Yale</University>
+                                <Semester>6</Semester>
+                                <Degree>Computer Science<Degree>
+                            </Student>
+                            <Student>
+                                <Name>Carla</Name>
+                                <Age>17</Age>
+                                <University>Yale</University>
+                                <Semester>1</Semester>
+                                <Degree>Biology<Degree>
+                            </Student>
+                            <Student>
+                                <Name>Leyla</Name>
+                                <Age>19</Age>
+                                <University>Beijing Tech</University>
+                                <Semester>3</Semester>
+                                <Degree>History<Degree>    
+                            </Student>
+                            <Student>
+                                <Name>Frank</Name>
+                                <Age>25</Age>
+                                <University>Beijing Tech</University>
+                                <Semester>10</Semester>
+                                <Degree>Chemistry<Degree>
+                            </Student>                   
+                            <Student>
+                                <Name>Garrett</Name>
+                                <Age>25</Age>
+                                <University>Fort Lewis College</University>
+                                <Semester>10</Semester>
+                                <Degree>History<Degree>
+                            </Student>
+                        </Students>";
+
+            //grab xml with XDocument and parse the xml with Xdocument.Parse
+            XDocument xStudents = new XDocument();
+            xStudents = XDocument.Parse(studentsXML);
+
+            //create a new collection based off of entered xml values
+            var students = from student in xStudents.Descendants("Student")
+                           select new
+                           {
+                               Name = student.Element("Name").Value,
+                               Age = student.Element("Age").Value,
+                               University = student.Element("University").Value
+                           };
+
+            foreach (var student in students)
+            {
+                Console.WriteLine($"Student {student.Name} is {student.Age} " +
+                    $"and goes to {student.University}");
+            }
+
+
+
+            var sortedStudents = from student in students
+                                 orderby student.Age
+                                 select student;
+
+            Console.WriteLine("Students sorted by age: ");
+
+            foreach (var student in sortedStudents)
+            {
+                Console.WriteLine($"Student {student.Name} is {student.Age} " +
+                    $"and goes to {student.University}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
