a70751b118e8029b70ba07618484c7aacd95ae97
Zachary Dowd <zacharyd2796@zohomail.com>
2021-11-17 16:45:31 -0700

    More Linq work

--- LinqToSqlReal/LinqToSqlReal/MainWindow.xaml.cs
+++ LinqToSqlReal/LinqToSqlReal/MainWindow.xaml.cs
@@ -28,18 +28,71 @@ namespace LinqToSqlReal
 
             string connection = ConfigurationManager.ConnectionStrings["LinqToSqlReal.Properties.Settings.C_DBCLASSConnectionString"].ConnectionString;
             dataContext = new LinqToSqlDataClassesDataContext(connection);
-            InsertUnis();
+            //InsertUnis();
+            //InsertStudents();
+            InsertLectures();
         }
 
         public void InsertUnis()
         {
+
+            dataContext.ExecuteCommand("delete from University");
+
             University harvard = new University();
             harvard.Name = "Harvard";
-            dataContext.Universities.InsertOnSubmit(harvard);
+            dataContext.Universities.InsertOnSubmit(harvard);  
+            
+            University cambridge = new University();
+            cambridge.Name = "Cambridge";
+            dataContext.Universities.InsertOnSubmit(cambridge);
 
             dataContext.SubmitChanges();
 
             MainDataGrid.ItemsSource = dataContext.Universities;
+        }
+
+        public void InsertStudents()
+        {
+            University harvard = dataContext.Universities.First(un => un.Name.Equals("Harvard"));
+            University cambridge = dataContext.Universities.First(un => un.Name.Equals("Cambridge"));
+
+
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student { Name = "Zack", Gender = "Male", UniversityId = harvard.Id});
+            students.Add(new Student { Name = "Allie", Gender = "Female", UniversityId = harvard.Id});
+            students.Add(new Student { Name = "Bennet", Gender = "Male", UniversityId = cambridge.Id});
+            students.Add(new Student { Name = "Christina", Gender = "Female", University = cambridge});
+
+            dataContext.Students.InsertAllOnSubmit(students);
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.Students;
+
+        }
+
+        public void InsertLectures()
+        {
+            dataContext.Lectures.InsertOnSubmit(new Lecture { Name = "Math" });
+            dataContext.Lectures.InsertOnSubmit(new Lecture { Name = "History" });
+            dataContext.Lectures.InsertOnSubmit(new Lecture { Name = "English" });
+
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource= dataContext.Lectures;
+        }
+
+        public void InsertStudentLectureAssociations()
+        {
+            Student Zack = dataContext.Students.First(st => st.Name.Equals("Zack"));
+            Student Allie = dataContext.Students.First(st => st.Name.Equals("Allie"));
+            Student Christina = dataContext.Students.First(st => st.Name.Equals("Christina"));
+            Student Bennet = dataContext.Students.First(st => st.Name.Equals("Bennet"));
+
+            Lecture Math = dataContext.Lectures.First(le => le.Name.Equals("Math"));
+            Lecture History = dataContext.Lectures.First(le => le.Name.Equals("History"));
+            Lecture English = dataContext.Lectures.First(le => le.Name.Equals("English"));
+            Lecture Science = dataContext.Lectures.First(le => le.Name.Equals("Science"));
         }
     }
 }
