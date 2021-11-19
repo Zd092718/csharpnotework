161732fd0c13c0a0c1d57517dc432846d1d1b25e
Zachary Dowd <zacharyd2796@zohomail.com>
2021-11-19 09:02:43 -0700

    more practice

---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int price = 100;
+            // prints specific currency
+            System.Diagnostics.Debug.WriteLine("The Item costs {0:C}", price);
+
+            StringBuilder stringBuilder = new StringBuilder();
+            ////Appends to string on current line
+            //for (int i = 0; i < 4; i++)
+            //{
+
+            //stringBuilder.Append(" nice to see you");
+            //}
+            //string s = stringBuilder.ToString();
+            //Console.WriteLine(s);
+            //stringBuilder.Clear();
+
+            ////Appends to following lines
+            //stringBuilder.AppendLine("Hello World");
+            //stringBuilder.AppendLine("Hello World");
+            //Console.WriteLine(stringBuilder.ToString());
+        }
+    }
+}
